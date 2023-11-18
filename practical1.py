ad3186fb5ee86a780e0e4c63668e57db5fe7fc6a
Amrendra <118886890+Amrendra2004@users.noreply.github.com>
2023-11-18 18:32:23 +0530

    Update practical1.py

--- practical1.py
+++ practical1.py
@@ -1,15 +1,44 @@
-from numpy import arange
-from scipy.optimize import line_search
-from matplotlib import pyplot
 
-def objective(x):
-	return (-5.0 + x)**2.0
+import numpy as np
 
-def gradient(x):
-	return 2.0 * (-5.0 + x)
+def line_search(f, df, x0, direction, alpha=0.1, beta=0.5):
+    step_size = alpha
+    while f(x0 + step_size * direction) > f(x0) - step_size * df(x0) @ direction:
+        step_size *= beta
 
-point = -5.0
-direction = -3.0
-print('start=%.1f, direction=%.1f' % (point, direction))
-result = line_search(objective, gradient, point, direction)
-print('Alpha: %s' % result[0])
+    return step_size
+
+def gradient_descent(f, df, x0, max_iters=100, tol=1e-6):
+
+    x_opt = x0
+    f_opt = f(x0)
+
+    for i in range(max_iters):
+        direction = -df(x_opt)
+        step_size = line_search(f, df, x_opt, direction)
+
+        x_new = x_opt + step_size * direction
+        f_new = f(x_new)
+
+        if np.linalg.norm(x_new - x_opt) < tol or np.abs(f_new - f_opt) < tol:
+            break
+
+        x_opt = x_new
+        f_opt = f_new
+
+    return x_opt, f_opt
+
+# Example usage
+def f(x):
+    return x**2 + 2 * x + 3
+
+def df(x):
+    return 2 * (x + 1)
+
+x0 = np.array([1.0])
+x_opt, f_opt = gradient_descent(f, df, x0)
+
+print("Optimal solution:", x_opt)
+print("Optimal function value:", f_opt)
+
+
